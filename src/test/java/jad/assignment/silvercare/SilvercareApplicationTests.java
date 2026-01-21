bf164a77ac875f454cc8143340b54a0a117d217b
Wolfyre243 <wolfyreblueflare@gmail.com>
2026-01-21 18:10:00 +0800

    first commit

---
+++ src/test/java/jad/assignment/silvercare/SilvercareApplicationTests.java
@@ -0,0 +1,13 @@
+package jad.assignment.silvercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilvercareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
